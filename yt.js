d3c70b577265b08b8d35c2998e69b6e35f54fbea
Neha-Bedi <18nehabedi@gmail.com>
2021-04-23 14:23:43 +0530

    first commit

---
+++ yt.js
@@ -0,0 +1,75 @@
+let puppeteer = require("puppeteer");
+
+let url = process.argv[2];
+let page;
+(async function(){
+let browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"],
+});
+
+let pages = await browser.pages();
+page = pages[0];
+
+await page.goto(url);
+let reqTime = await page.evaluate(async function () {
+
+    function hmsToSecondsOnly(str) {
+        var p = str.split(':'),
+            s = 0, m = 1;
+    
+        while (p.length > 0) {
+            s += m * parseInt(p.pop(), 10);
+            m *= 60;
+        }
+    
+        return s;
+    }
+
+    let tVids =  document.querySelector("#stats .style-scope.yt-formatted-string");
+    let totalvids = Number(tVids.innerText);
+    let scrolls = Math.ceil(totalvids / 100);
+let tVal = await new Promise(function(resolve,reject){
+    let interval =setInterval(function()
+    {
+        let vids = document.querySelectorAll(".yt-simple-endpoint.style-scope.ytd-playlist-video-renderer");
+        vids[vids.length-1].scrollIntoView();
+
+
+        if(Math.ceil (vids.length /100)== scrolls){
+        clearInterval(interval);
+        let timeArr = document.querySelectorAll("span.style-scope.ytd-thumbnail-overlay-time-status-renderer");
+        let totalTime = 0;
+        for(let i = 0; i < timeArr.length; i++){
+            totalTime += hmsToSecondsOnly(timeArr[i].innerText);
+        }
+        console.log(totalTime / (60*60));
+        resolve(totalTime);
+        }
+    }, 5000); 
+});
+return tVal;
+});
+let totalSeconds = reqTime;
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+console.log("1x: Normal speed " + hours + "h : " + minutes + "m : " + seconds + "s");
+
+totalSeconds = reqTime/2;
+hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+minutes = Math.floor(totalSeconds / 60);
+seconds = totalSeconds % 60;
+console.log("2x: Faster speed " + hours + "h : " + minutes + "m : " + seconds + "s");
+
+totalSeconds = reqTime/5;
+hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+minutes = Math.floor(totalSeconds / 60);
+seconds = totalSeconds % 60;
+console.log("5x: Faster speed " + hours + "h : " + minutes + "m : " + seconds + "s");
+
+})();
